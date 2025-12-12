67f19ae4f167b200122f4ad9bcbddaaf179a04af
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2025-12-12 15:55:28 +0100

    Run ESLint/Prettier on files

--- src/home-load.js
+++ src/home-load.js
@@ -1,23 +1,23 @@
 export function pageLoad() {
-  const content = document.querySelector("#content")
-  const restName = document.createElement("div")
-  restName.id = "restaurant-name"
-  const h1 = document.createElement("h1")
-  h1.textContent = `Martuteneko Jatextea`
-  
-  restName.appendChild(h1)
-  
-  const restIntro = document.createElement("div")
-  restIntro.className = "restaurant-intro"
-  const p1 = document.createElement("p")
-  p1.textContent = `Welcome to Martuteneko Jatetxea, a cozy corner where tradition meets flavor in the heart of the Basque Country. Our kitchen celebrates seasonal ingredients, local producers, and the timeless recipes that make Basque gastronomy world‑renowned. Whether you’re joining us for a casual lunch, a family gathering, or a special evening, we invite you to savor dishes crafted with care and passion.`
-  
-  const p2 = document.createElement("p")
-  p2.textContent = `Come in, take a seat, and let Martuteneko Jatetxea be your home for unforgettable meals and warm hospitality.`
-  restIntro.appendChild(p1)
-  restIntro.appendChild(p2)
-  
-  content.appendChild(restName)
-  content.appendChild(restIntro)
+  const content = document.querySelector('#content');
+  const restName = document.createElement('div');
+  restName.id = 'restaurant-name';
+  const h1 = document.createElement('h1');
+  h1.textContent = `Martuteneko Jatextea`;
+
+  restName.appendChild(h1);
+
+  const restIntro = document.createElement('div');
+  restIntro.className = 'restaurant-intro';
+  const p1 = document.createElement('p');
+  p1.textContent = `Welcome to Martuteneko Jatetxea, a cozy corner where tradition meets flavor in the heart of the Basque Country. Our kitchen celebrates seasonal ingredients, local producers, and the timeless recipes that make Basque gastronomy world‑renowned. Whether you’re joining us for a casual lunch, a family gathering, or a special evening, we invite you to savor dishes crafted with care and passion.`;
+
+  const p2 = document.createElement('p');
+  p2.textContent = `Come in, take a seat, and let Martuteneko Jatetxea be your home for unforgettable meals and warm hospitality.`;
+  restIntro.appendChild(p1);
+  restIntro.appendChild(p2);
+
+  content.appendChild(restName);
+  content.appendChild(restIntro);
 }
-pageLoad()
+pageLoad();
--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
-import "./styles.css";
-import "./normalize.css";
-import { pageLoad } from "./home-load.js";
-import { menuLoad } from "./menu-load.js";
-import { aboutLoad } from "./about-load.js";
+import './styles.css';
+import './normalize.css';
+import { pageLoad } from './home-load.js';
+import { menuLoad } from './menu-load.js';
+import { aboutLoad } from './about-load.js';
 
 // DOM elemnents
-const content = document.querySelector("#content")
-const home = document.querySelector("#home")
-const menu = document.querySelector("#menu")
-const about = document.querySelector("#about")
+const content = document.querySelector('#content');
+const home = document.querySelector('#home');
+const menu = document.querySelector('#menu');
+const about = document.querySelector('#about');
 
-home.addEventListener("click", () =>{
-  clearContent()
-  pageLoad()
-})
+home.addEventListener('click', () => {
+  clearContent();
+  pageLoad();
+});
 
-menu.addEventListener("click", () =>{
-  clearContent()
-  menuLoad()
-})
+menu.addEventListener('click', () => {
+  clearContent();
+  menuLoad();
+});
 
-about.addEventListener("click", () =>{
-  clearContent()
-  aboutLoad()
-})
-
+about.addEventListener('click', () => {
+  clearContent();
+  aboutLoad();
+});
 
 function clearContent() {
-  content.textContent = "";
+  content.textContent = '';
 }
 
-clearContent()
-pageLoad()
+clearContent();
+pageLoad();
